a9bc303811a665f044826614158f9269ab40e795
emtushar <72194068+emtushar@users.noreply.github.com>
2024-01-24 19:32:31 +0000

    tried array join method

--- array.js
+++ array.js
@@ -4,5 +4,8 @@ const oneArray = firstArray.slice(1,4);
 const twoArray = secondArray.splice(1,4);
 console.log(firstArray,secondArray,oneArray,twoArray)
 
+console.log(firstArray.join(","));
+console.log(firstArray);
+
 // splice does include the end value but it will manipulate/modify the original array
 // slice does not include the end value it will not affect the original array
