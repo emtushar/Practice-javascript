d9a7d3cc0326cabb44c7f41f925d5dfa6814cbb7
emtushar <72194068+emtushar@users.noreply.github.com>
2024-01-24 19:17:07 +0000

    first commit

---
+++ array.js
@@ -0,0 +1,8 @@
+const firstArray = [3,7,2,9,1,4];
+const secondArray = [1,5,2,6,8,3];
+const oneArray = firstArray.slice(1,4);
+const twoArray = secondArray.splice(1,4);
+console.log(firstArray,secondArray,oneArray,twoArray)
+
+// splice does include the end value but it will manipulate/modify the original array
+// slice does not include the end value it will not affect the original array
